16334870fa9bdf30d7b3716a9d9fe357a853993e
Guilherme Costa <gui-costaquino@hotmail.com>
2022-06-07 20:29:17 -0300

    Adicionando questões 4 e 5 do livro Chagas, Baruque & Baruque (2010)

---
+++ CBBAtividadePratica5.java
@@ -0,0 +1,11 @@
+public class CBBAtividadePratica5{
+    public static void main(String[] args){
+        System.out.println("Os números pares de 0 a 100:");
+        int i;
+        for(i=0; i<=100; i++){
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
+## Resolução
 
+##### Livro Chagas, Baruque & Baruque (2010)
+4) A resolução encontra-se no arquivo CBBAtividadePratica4.java
+5) A resolução encontra-se no arquivo CBBAtividadePratica5.java
+
+##### Livro Batista & Moraes (2013)
+6)
+7)
+8)
+9)
+10)
